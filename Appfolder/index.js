99cb50df60d73d2d393272e59956dda0df2903af
Alex McDonald <acm4219@gmail.com>
2020-09-12 17:06:18 -0500

    Added gitignore and generate stuff

--- Appfolder/index.js
+++ Appfolder/index.js
@@ -1,74 +1,63 @@
 // array of questions for user
 let inquirer = require("inquirer");
 let fs = require("fs");
+let generate = require("./utils/generateMarkdown");
 const questions = [
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is the title of your read me?",
-        name: "README Title",
-      },
-      {
-        type: "input",
-        message: "What is the description of the Application?",
-        name: "Description",
-      },
-      {
-        type: "input",
-        message: "What is the table of Contents?",
-        name: "Table of Contents",
-      },
-      {
-        type: "input",
-        message: "How do you install the app?",
-        name: "Installation",
-      },
-      {
-        type: "input",
-        message: "How do you use it?",
-        name: "Usage",
-      },
-      {
-        type: "input",
-        message: "License?",
-        name: "License",
-      },
-      {
-        type: "input",
-        message: "Who contributed to the project?",
-        name: "Contributing",
-      },
-      {
-        type: "input",
-        message: "How often did you test this?",
-        name: "tests",
-      },
-      {
-        type: "input",
-        message: "What are some questions you need to ask?",
-        name: "Questions",
-      },
-    ])
-    .then(function (response) {
-      if (response.confirm === response.input) {
-        console.log("Success!");
-      } else {
-        console.log("Unable to process!");
-      }
-    }),
+  {
+    type: "input",
+    message: "What is the title of your read me?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the description of the Application?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How do you install the app?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "How do you use it?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "License?",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Who contributed to the project?",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "How often did you test this?",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "What are some questions you need to ask?",
+    name: "questions",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile("README.md", process.argv[2], function (err) {
+// function writeToFile(fileName, data) {
+
+// }
+inquirer.prompt(questions).then(function (data) {
+  fs.writeFile("README.md", generate(data), function (err) {
     if (err) {
       return console.log(err);
     } else {
       console.log("success");
     }
   });
-}
+});
 
 // function to initialize program
 function init() {}
--- Appfolder/utils/generateMarkdown.js
+++ Appfolder/utils/generateMarkdown.js
@@ -1,7 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - ### [Description](#Description)
+  - ### [Installation](#Installation)
+  - ### [Usage](#Usage)
+  - ### [License](#License)
+  - ### [Contributors](#Contributors)
+  - ### [Test](#Test)
+  - ### [Questions](#Question)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${data.license}
+  ## Contributors
+  ${data.contributing}
+  ## Test
+  ${data.test}
+  ## Questions
+  ${data.questions}
 `;
 }
 
