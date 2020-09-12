5bf4c3e413d11fd09b9e688bad0ad613a1a6a55e
Alex McDonald <acm4219@gmail.com>
2020-09-11 19:26:52 -0500

    added some code to the write function still sorting that out

--- Appfolder/index.js
+++ Appfolder/index.js
@@ -1,57 +1,74 @@
 // array of questions for user
 let inquirer = require("inquirer");
+let fs = require("fs");
 const questions = [
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the title of your read me?",
-      name: "README Title",
-    },
-    {
-      type: "input",
-      message: "What is the description of the Application?",
-      name: "Description",
-    },
-    {
-      type: "input",
-      message: "What is the table of Contents?",
-      name: "Table of Contents",
-    },
-    {
-      type: "input",
-      message: "How do you install the app?",
-      name: "Installation",
-    },
-    {
-      type: "input",
-      message: "How do you use it?",
-      name: "Usage",
-    },
-    {
-      type: "input",
-      message: "License?",
-      name: "License",
-    },
-    {
-      type: "input",
-      message: "Who contributed to the project?",
-      name: "Contributing",
-    },
-    {
-      type: "input",
-      message: "How often did you test this?",
-      name: "tests",
-    },
-    {
-      type: "input",
-      message: "What are some questions you need to ask?",
-      name: "Questions",
-    },
-  ]),
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of your read me?",
+        name: "README Title",
+      },
+      {
+        type: "input",
+        message: "What is the description of the Application?",
+        name: "Description",
+      },
+      {
+        type: "input",
+        message: "What is the table of Contents?",
+        name: "Table of Contents",
+      },
+      {
+        type: "input",
+        message: "How do you install the app?",
+        name: "Installation",
+      },
+      {
+        type: "input",
+        message: "How do you use it?",
+        name: "Usage",
+      },
+      {
+        type: "input",
+        message: "License?",
+        name: "License",
+      },
+      {
+        type: "input",
+        message: "Who contributed to the project?",
+        name: "Contributing",
+      },
+      {
+        type: "input",
+        message: "How often did you test this?",
+        name: "tests",
+      },
+      {
+        type: "input",
+        message: "What are some questions you need to ask?",
+        name: "Questions",
+      },
+    ])
+    .then(function (response) {
+      if (response.confirm === response.input) {
+        console.log("Success!");
+      } else {
+        console.log("Unable to process!");
+      }
+    }),
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile("README.md", process.argv[2], function (err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log("success");
+    }
+  });
+}
 
 // function to initialize program
 function init() {}
